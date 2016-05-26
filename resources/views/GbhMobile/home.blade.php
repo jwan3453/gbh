e41dc62ec12affba3e5ff742a58bf74c22c81d0e
jwan3453 <jwan3453@uni.sydney.edu.au>
2016-05-26 12:08:46 +0800

    添加首页日历功能

--- resources/views/GbhMobile/home.blade.php
+++ resources/views/GbhMobile/home.blade.php
@@ -127,15 +127,23 @@
     <div class="date-box">
         <div class="select-date auto-margin">
             <div class="f-left"><i class="ui icon calendar"></i></div>
-            <span class="from-date">2015-5-20</span>
+            <span class="from-date">入住时间</span>
             至
-            <span class="to-date">2015-05-27</span>
+            <span class="to-date">离开时间</span>
         </div>
 
-        <div class="calendar">
+        <div class="calendar" id="calendar">
+
+            <div class="select-month">
+                <i class="angle left icon f-left big pre-month"></i>
+                <span></span>
+                <i class="angle right icon f-right big next-month"></i>
+            </div>
+
+
             <table>
                 <tr class="t-header">
-                    <th>天</th>
+                    <th>日</th>
                     <th>一</th>
                     <th>二</th>
                     <th>三</th>
@@ -199,8 +207,10 @@
                 <tr>
             </table>
         </div>
-
     </div>
+
+
+    <div class=" regular-btn red-btn auto-margin">搜索</div>
 
 </div>
 
@@ -243,9 +253,30 @@
 
             //获取时间
             var date = new Date(),
-            year= date.getFullYear(),
-            month = date.getMonth() + 1,
-            day = date.getDate(),days =0;
+            currentYear= date.getFullYear(),
+            currentMonth = date.getMonth() + 1,
+            today = date.getDate(),days = 0;
+
+
+
+            var newYear = date.getFullYear(),
+                newMonth =date.getMonth() + 1;
+
+
+            var   selectedDateType= 0; //选择的状态(是选择 1.到店时间 还是 2.离店时间)
+
+            //选择的到店时间的年月日
+            var  selectedFromYear = 0;
+            var  selectedFromMonth = 0;
+            var  selectedFromDay = 0;
+
+            //选择离店时间的年月日
+            var  selectedToYear = 0;
+            var  selectedToMonth = 0;
+            var  selectedToDay = 0;
+
+
+
 
 
             //初始化日历
@@ -282,13 +313,21 @@
 
 
 
-                for(var i=0; i<42; i++)
+                //显示月份
+
+                $('.select-month').find('span').text(year +'年'+ month + '月');
+
+
+                //显示日期
+                for(var i=0; i<36; i++)
                 {
                     $td.eq(i).text('');
+                    $td.removeClass('disable-day');
+                    $td.removeClass('selected-day')
                 }
 
                 var index= 0;
-                for(var i = 0;i < 42; i++){
+                for(var i = 0;i <days; i++){
 
 
                     if(i >= (days))
@@ -297,35 +336,206 @@
                     }
                     else
                     {
-                        if(i<(day-1))
+
+                        if(i<(today-1) && currentMonth === month)
                         {
                             $td.eq( week % 7 +i).addClass('disable-day');
                         }
+<<<<<<< Updated upstream
 
                         if(i===(day-1))
+=======
+                        else if(currentMonth > month )
+>>>>>>> Stashed changes
                         {
-                            $td.eq( week % 7 +i).addClass('selected-day');
+                            $td.eq( week % 7 +i).addClass('disable-day');
                         }
+//                        if(i==(day-1))
+//                        {
+//                            $td.eq( week % 7 +i).addClass('selected-days');
+//                        }
                         index =i;
                     }
 
-                    $td.eq( week % 7 +i).text(index+1);
+                    //判断是否为今天的day
+//                    if(i<27 && i === (day-1))
+//                    {
+//                        $td.eq( week % 7 +i).text('今天');
+//                    }
+//                    else{
+//
+//                    }
+                    //是否有选择过到店日期
+                    if(selectedFromYear == year && selectedFromMonth == month && selectedFromDay === (i+1) )
+                    {
+                        $td.eq( week % 7 +i).html('' +
+                                '<span class=\'date\'>'+(index+1)+'</span>' +
+                                '<span class=\'status\'>入住</span>').find('.date').addClass('selected-day');
+
+                    }
+                    //是否有选择过离店日期
+                    else if(selectedToYear == year && selectedToMonth == month && selectedToDay === (i+1)  )
+                    {
+                        $td.eq( week % 7 +i).html('' +
+                                '<span class=\'date\'>'+(index+1)+'</span>' +
+                                '<span class=\'status\'>离店</span>').find('.date').addClass('selected-day');
+                    }
+                    //普通日期
+                    else{
+                        $td.eq( week % 7 +i).html('<span class=\'date\'>'+(index+1)+'</span>');
+                    }
+
+                }
+
+                return week;
+            }
+
+
+
+
+
+            var week= initCal(currentYear,currentMonth,today);
+
+            var fromDate = '';//到店日期 string zhuan date
+            var toDate =''; //离店日期
+
+            $('.calendar').find('td').click(function(){
+                if(!$(this).hasClass('disable-day') && $(this).text()!=='' )
+                {
+                    var newDay = parseInt($(this).text());
+
+
+
+                    //如果没有选择开始日期, 那么把这个日期选为开始日期
+                    if(selectedDateType === 1)
+                    {
+                        selectedFromYear = newYear;
+                        selectedFromMonth = newMonth;
+                        selectedFromDay = newDay;
+                         fromDate =new Date(selectedFromYear +'-'+selectedFromMonth+'-'+selectedFromDay);
+                         toDate = new Date(selectedToYear +'-'+selectedToMonth+'-'+selectedToDay) ;
+
+                        if(   fromDate > toDate)
+                        {
+
+                            alert('yes');
+                        }
+
+
+                        $('.selected-from-date').html(
+                                '<span class=\'date\'>'+
+                                    $('.selected-from-date').find('.date').text()+
+                                '</span>').removeClass('selected-from-date'); //$(this).);
+
+
+                        $td.eq( week % 7 +(newDay-1)).html('' +
+                                '<span class=\'date\'>'+(newDay)+'</span>' +
+                                '<span class=\'status\'>入住</span>').addClass('selected-from-date').find('.date').addClass('selected-day ');
+                        //hasSelectedStartDay = true;
+
+                        $('.from-date').text(selectedFromYear+'-'+selectedFromMonth+'-'+newDay);
+                        //$('#calendar') .transition('horizontal flip');
+                    }
+                    //如果没有选择离店日期, 那么把这个日期选为离店日期
+                    if(selectedDateType === 2){
+
+                        selectedToYear = newYear;
+                        selectedToMonth = newMonth;
+                        selectedToDay =  newDay;
+                        fromDate =new Date(selectedFromYear +'-'+selectedFromMonth+'-'+selectedFromDay);
+                        toDate = new Date(selectedToYear +'-'+selectedToMonth+'-'+selectedToDay) ;
+
+                        if(   fromDate > toDate)
+                        {
+
+                            alert('yes');
+                        }
+
+                        $('.selected-to-date').html(
+                                '<span class=\'date\'>'+
+                                $('.selected-to-date').find('.date').text()+
+                                '</span>').removeClass('selected-to-date'); //$(this).);
+
+
+                        $td.eq( week % 7 +(newDay-1)).html('' +
+                                '<span class=\'date\'>'+(newDay)+'</span>' +
+                                '<span class=\'status\'>离店</span>').addClass('selected-to-date').find('.date').addClass('selected-day ');
+                        //hasSelectedStartDay = true;
+
+
+                        $('.to-date').text(selectedToYear+'-'+selectedToMonth+'-'+newDay);
+                    }
+
+
+
+                }
+            })
+
+            $('.next-month').click(function(){
+
+                //下个月
+                newMonth += 1;
+                week= initCal(newYear,newMonth,today);
+
+            })
+            $('.pre-month').click(function(){
+                //上个月
+                newMonth -= 1;
+                week= initCal(newYear,newMonth,today);
+
+            })
+
+            $('.from-date').click(function(){
+                //跳到入住日历
+                if(selectedFromDay !== 0 )
+                {
+                   // alert();
+                    week=initCal(selectedFromYear,selectedFromMonth,selectedFromDay);
+                    newYear = selectedFromYear;
+                    newMonth =  selectedFromMonth;
+
+                }
+                selectedDateType =1;
+                $('.to-date').removeClass('select-date-click');
+                $(this).addClass('select-date-click');
+                //日历闪烁动效
+                $('.selected-from-date').find('.date').transition('scale').transition('scale');
+                if($('#calendar').css('display') === 'none')
+                {
+                    $('#calendar') .transition('horizontal flip');
                 }
 
 
-            }
-            initCal(year,month,day);
+            })
 
-
+            $('.to-date').click(function(){
+                //跳到离店日历
+                if(selectedToDay !== 0 )
+                {
+                    week=initCal(selectedToYear,selectedToMonth,selectedToDay);
+                    newYear = selectedToYear;
+                    newMonth =  selectedToMonth;
+                }
+                selectedDateType =2;
+                $('.from-date').removeClass('select-date-click');
+                //日历闪烁动效
+                $('.selected-to-date').find('.date').transition('scale').transition('scale');
+                $(this).addClass('select-date-click');
+                if($('#calendar').css('display') === 'none')
+                {
+                    $('#calendar') .transition('horizontal flip');
+                }
+            })
 
         })
 
         $(document).ready(function(){
-                $('#search').focus(function(){
-                    $('.ui.sidebar')
-                            .sidebar('toggle');
-                    $('#search').blur();
-                })
+            $('#search').focus(function(){
+                            $('.ui.sidebar')
+                                    .sidebar('toggle');
+                            $('#search').blur();
+                        })
+
         })
     </script>
 @stop
