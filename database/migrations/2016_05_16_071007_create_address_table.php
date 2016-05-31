760a403cacf3eaaca6a7a75f85827d18eaa6f7d8
18759758958 <18759758958@163.com>
2016-05-31 19:46:54 +0800

    后台首页

--- database/migrations/2016_05_16_071007_create_address_table.php
+++ database/migrations/2016_05_16_071007_create_address_table.php
@@ -19,7 +19,7 @@ class CreateAddressTable extends Migration
             $table->integer('district_id')->default(0);
             $table->string('detailed',100);
             $table->string('detailed_english',100);
-            $table->stirng('longtude',15);//经度
+            $table->string('longtitute',15);//经度
             $table->string('latitude',15);//纬度
             
             $table->timestamps();
